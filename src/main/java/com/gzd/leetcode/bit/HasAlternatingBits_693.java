7f7e683bc8f8774d322b2570876f2d5ec65a38fc
guozhongdong <guozhongdong@jd.com>
2022-10-01 14:49:32 +0800

    汉明距离

---
+++ src/main/java/com/gzd/leetcode/bit/HasAlternatingBits_693.java
@@ -0,0 +1,68 @@
+package com.gzd.leetcode.bit;
+
+import javax.xml.stream.FactoryConfigurationError;
+
+/**
+ *
+ * 给定一个正整数，检查它的二进制表示是否总是 0、1 交替出现：
+ * 换句话说，就是二进制表示中相邻两位的数字永不相同。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：n = 5
+ * 输出：true
+ * 解释：5 的二进制表示是：101
+ * 示例 2：
+ *
+ * 输入：n = 7
+ * 输出：false
+ * 解释：7 的二进制表示是：111.
+ * 示例 3：
+ *
+ * 输入：n = 11
+ * 输出：false
+ * 解释：11 的二进制表示是：1011.
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= n <= 231 - 1
+ *
+ * 定义一个变量boolean
+ * 碰到1变true，碰到0变false
+ *
+ *
+ *
+ * @author guozhongdong
+ * @date 2022/10/1 13:18
+ */
+public class HasAlternatingBits_693 {
+
+    public static void main(String[] args) {
+
+        HasAlternatingBits_693 bits = new HasAlternatingBits_693();
+        System.out.println(bits.hasAlternatingBits(5));
+
+    }
+
+    /**
+     * 取模求余，与前一个数比较是否相等
+     */
+    public boolean hasAlternatingBits(int n) {
+        // 1010
+        // 0011
+        int pre = 3;
+        while (n != 0){
+           int num = n % 2;
+           if (pre == num ){
+               return false;
+           }
+           pre = num;
+           n = n/2;
+
+        }
+        return true;
+    }
+}
